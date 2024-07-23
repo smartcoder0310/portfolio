dd5878e410f78dc2ee5489cf4918f9c95db943ac
smartcoder0310 <jamesmud217@gmail.com>
2024-07-23 17:10:43 -0400

    next-static-page-generator

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -4,8 +4,15 @@ import { Projects } from "@/components/main/projects";
 import { Career } from "@/components/main/career";
 import { Skills } from "@/components/main/skills";
 
+export async function generateStaticParams() {
+  // Return an array of possible dynamic parameters
+  return [
+    { slug: 'example' },
+    // Add other static paths if necessary
+  ];
+}
 
-export default function Home() {
+export default function Page({ params }: { params: { slug: string } }) {
   return (
     <main className="h-full w-full">
       <div className="flex flex-col gap-20">
--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true, // Enable support for the new `app` directory (if necessary)
-    },
-    trailingSlash: true, // Add trailing slashes to URLs for GitHub Pages
-    async generateStaticParams(defaultPathMap) {
-        return {
-        '/': { page: '/' },
-        // Define additional paths as needed
-        }
-    },
+    trailingSlash: true
 }
 
 module.exports = nextConfig

a184710ccaa0c2198be71e7c2c74a85f12d68a28
smartcoder0310 <jamesmud217@gmail.com>
2024-07-23 17:01:31 -0400

    predeploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        appDir: true, // Enable support for the new `app` directory (if necessary)
+    },
+    trailingSlash: true, // Add trailing slashes to URLs for GitHub Pages
+    async exportPathMap(defaultPathMap) {
+        return {
+        '/': { page: '/' },
+        // Define additional paths as needed
+        }
+    },
+}
 
 module.exports = nextConfig
